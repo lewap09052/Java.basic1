1b1410750e3782f7af78be27057f444ee31973bd
lewap09052 <pawelniemiec1@msn.com>
2022-09-19 20:03:38 +0100

    added new class main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadz wartosc x");
+        int x = scanner.nextInt();
+        System.out.println("Wprowadz wartosc y");
+        int y = scanner.nextInt();
+        int z = x+y;
+        System.out.println( " Wynik dodawania to:"+z);
     }
 }
