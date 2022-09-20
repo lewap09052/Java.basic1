4f358223b0e69e5b11656f13b16398241e3c24b5
lewap09052 <pawelniemiec1@msn.com>
2022-09-20 14:41:47 +0100

    added new class main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+
+        System.out.println("WITAJ!");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wprowadz wartosc x");
+        System.out.println("Enter your name:");
+        String name = scanner.next();
+        System.out.println("Enter your last name:");
+        String lastName = scanner.next();
+        System.out.println("How old are You?");
         int x = scanner.nextInt();
-        if (x<3) {
-            System.out.println("x jest mniejsze od 3");
-        }else {
-            System.out.println("x jest wieksze od 3");
+        if (x<18) {
+            System.out.println(name + " " + lastName + ", jestes za mlody zeby wejsc!");
+        } else {
+            System.out.println(name + " " + lastName + ", mozesz wejsc ;)!");
         }
 
-    }
-}
+
+    }}
